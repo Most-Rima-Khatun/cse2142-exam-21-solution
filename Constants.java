dbcdd5e08d5a0282a18afd892b0c9e2097e42f60
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2024-10-05 10:05:39 +0600

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,20 @@
+public class Constants{
+    public static final String EMPTY = "";
+    public static final String STUDENT_TEXT_FILE = "students.txt";
+    public static final String SPLIT = ", ";
+    public static final String LIST_LAST_UPDATED = "\nList last updated on ";
+    public static final String USAGE_WORD = "Usage:(a|r|c + WORD|? WORD)";
+    public static final String ALL_STUDENT_LIST_SHAW = "a";
+    public static final String LOADING_DATA = "Loading data ...";
+    public static final String DATA_LOADED = "Data Loaded.";
+    public static final String RANDOM_STUDENT_LIST_SHAW = "r";
+    public static final String ADD = "+";
+    public static final String DATEFORMAT = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String QUREY = "?";
+    public static final String FINDOUT = "We found it!";
+    public static final String COUNT_STUDENT = "c";
+    public static final char SPACE = ' ';
+    public static final String WORD_FOUND = " word(s) found ";
+    public static final String  WRONG_ARGUMENTS = "Wrong arguments pass";
+    public static final String INVALID_ARGUMENTS = "Invalid arguments";
+}
--- StudentList.java
+++ StudentList.java
@@ -3,120 +3,97 @@ import java.text.*;
 import java.util.*;
 
 public class StudentList {
+    //Create Reader methods for simpify this code by removing dublicates
+    public static String Reader(){
+    String line  = Constants.EMPTY;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(Constants.STUDENT_TEXT_FILE)));
+             line = bufferedReader.readLine();
+        } catch (Exception e) {
+        }
+        return line;
+    }
+    //Create Writer methods for simpify this code by removing dublicates
+    public static void Writer(String substring, String formatdate)
+    {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(
+                    new FileWriter(Constants.STUDENT_TEXT_FILE, true));
+            bufferedWriter.write(Constants.SPLIT + substring + Constants.LIST_LAST_UPDATED + formatdate);
+            bufferedWriter.close();
+        } catch (Exception e) {
+        }
+
+    }
     public static void main(String[] args) {
-
-        // Check arguments
-        if (args[0].equals("a")) {
-
-            System.out.println("Loading data ...");
-            try {
-                BufferedReader s =  new BufferedReader(
-                                    new InputStreamReader(
-                                    new FileInputStream("students.txt")));
-
-                String r = s.readLine();
-                String i[] = r.split(",");
-
-                for (String j : i) {
-                    System.out.println(j);
+        if(args == null || args.length !=1){
+            System.out.println(Constants.USAGE_WORD);
+        }
+        if(args.length>1){
+            System.out.println("greater than 1");
+        }
+//		Check arguments
+        else if (args[0].equals(Constants.ALL_STUDENT_LIST_SHAW)) {
+            System.out.println(Constants.LOADING_DATA);
+            Reader(); // Call Reader methods
+                String students[] = Reader().split(Constants.SPLIT);
+                for (String student : students) {
+                    System.out.println(student);
                 }
-            } catch (Exception e) {
-            }
-            System.out.println("Data Loaded.");
-        } else if (args[0].equals("r")) {
-            System.out.println("Loading data ...");
-                try {
-                    BufferedReader s =  new BufferedReader(
-                                        new InputStreamReader(
-                                        new FileInputStream("students.txt")));
 
-                    String r = s.readLine();
-                    System.out.println(r);
+            System.out.println(Constants.DATA_LOADED);
+        } else if (args[0].equals(Constants.RANDOM_STUDENT_LIST_SHAW)) {
+            System.out.println(Constants.LOADING_DATA);
+            Reader(); // Call Reader methods
+                System.out.println(Reader());
+                String students[] = Reader().split(Constants.SPLIT);
+                Random random = new Random();
+                int next = random.nextInt();
+                System.out.println(students[next]);
+            System.out.println(Constants.DATA_LOADED);
+        } else if (args[0].contains(Constants.ADD)) {
+            System.out.println(Constants.LOADING_DATA);
 
-                    String i[] = r.split(",");
-                    Random x = new Random();
+                String substring = args[0].substring(1);
+                Date date = new Date();
+                String dateformat = Constants.DATEFORMAT;
+                DateFormat dateFormat = new SimpleDateFormat(dateformat);
+                String formatdate = dateFormat.format(date);
+            Writer(substring,formatdate); // Call Writer methods passing arguments values
+            System.out.println(Constants.DATA_LOADED);
+        } else if (args[0].contains(Constants.QUREY)) {
+            System.out.println(Constants.LOADING_DATA);
+            Reader(); // Call Reader methods
+                String students[] = Reader().split(Constants.SPLIT);
 
-                    int y = x.nextInt();
-                    System.out.println(i[y]);
+                String substring = args[0].substring(1);
+                for (int idx = 0; idx < students.length ; idx++) {
+                    if (students[idx].equals(substring)) {
+                        System.out.println(Constants.FINDOUT);
 
-                } catch (Exception e) {
-            }
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("+")) {
-
-            System.out.println("Loading data ...");
-                try {
-                    BufferedWriter s =  new BufferedWriter(
-                                        new FileWriter("students.txt", true));
-
-                    String t = args[0].substring(1);
-                    Date d = new Date();
-
-                    String df = "dd/mm/yyyy-hh:mm:ss a";
-                    DateFormat dateFormat = new SimpleDateFormat(df);
-
-                    String fd = dateFormat.format(d);
-                    s.write(", " + t + "\nList last updated on " + fd);
-                    s.close();
-
-                } catch (Exception e) {
-            }
-
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("?")) {
-
-            System.out.println("Loading data ...");
-                try {
-                        BufferedReader s =  new BufferedReader(
-                                            new InputStreamReader(
-                                            new FileInputStream("students.txt")));
-
-                        String r = s.readLine();
-                        String i[] = r.split(",");
-
-                        boolean done = false;
-                        String t = args[0].substring(1);
-
-                        for (int idx = 0; idx < i.length && !done; idx++) {
-
-                            if (i[idx].equals(t)) {
-                                System.out.println("We found it!");
-                                done = true;
-                            }
-                        }
-                } catch (Exception e) {
+                    }
                 }
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("c")) {
+            System.out.println(Constants.DATA_LOADED);
+        } else if (args[0].contains(Constants.COUNT_STUDENT)) {
+            System.out.println(Constants.LOADING_DATA);
+            Reader(); // Call Reader methods
+                char words[] = Reader().toCharArray();
 
-            System.out.println("Loading data ...");
+                int count = 0;
+                //This for loop use for counting the number of students in list
+                for (char letter : words) {
+                    if (letter == Constants.SPACE) {
 
-                try {
-                        BufferedReader s =  new BufferedReader(
-                                            new InputStreamReader(
-                                            new FileInputStream("students.txt")));
+                            count++;
 
-                        String D = s.readLine();
-                        char a[] = D.toCharArray();
-
-                        boolean in_word = false;
-                        int count = 0;
-
-                        for (char c : a) {
-
-                            if (c == ' ') {
-                                if (!in_word) {
-                                    count++;
-                                    in_word = true;
-                                } else {
-                                    in_word = false;
-                                }
-                            }
-                        }
-            System.out.println(count + " word(s) found " + a.length);
-            } catch (Exception e) {
-            }
-            System.out.println("Data Loaded.");
+                    }
+                }
+                System.out.println(count/2 + Constants.WORD_FOUND + words.length);
+            System.out.println(Constants.DATA_LOADED);
+        }else {
+            System.out.println(Constants.INVALID_ARGUMENTS);
         }
     }
 }
